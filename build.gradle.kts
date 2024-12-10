c2da4bc7c626287e7479e91b3a653165de0b225e
Tom Ient <thomas.ient@digital.cabinet-office.gov.uk>
2024-12-10 16:30:31 +0000

    fix: Breaking Changes (#143)
    # DCMAW-10478: Breaking Changes
    
    - Update build.gradle.kts to include gradle extras as required by
    mobile-android-pipelines
    
    ## Evidence of the change
    
    - None
    
    ## Checklist
    
    ### Before creating the pull request
    
    - [x] Commit messages that conform to conventional commit messages.
    - [x] Ran the app locally ensuring it builds.
    - [x] Tests pass locally.
    - [x] Pull request has a clear title with a short description about the
    feature or update.
    - [x] Created a `draft` pull request if it's not ready for review.
    
    ### Before the CODEOWNERS review the pull request
    
    - [x] Complete all Acceptance Criteria within Jira ticket.
    - [x] Self-review code.
    - [x] Successfully run changes on a testing device.
    - [x] Complete automated Testing:
      * [x] Unit Tests.
      * [x] Integration Tests.
      * [x] Instrumentation / Emulator Tests.
    - [x] Review [Accessibility considerations].
    - [x] Handle PR comments.
    
    ### Before merging the pull request
    
    - [x] [Sonar cloud report] passes inspections for your PR.
    - [x] Resolve all comments.
    
    [Sonar cloud report]:
    https://sonarcloud.io/project/overview?id=di-mobile-android-ui
    [Accessibility considerations]:
    https://developer.android.com/guide/topics/ui/accessibility/testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,22 @@
 import uk.gov.pipelines.config.ApkConfig
+import uk.gov.pipelines.emulator.EmulatorConfig
+import uk.gov.pipelines.emulator.SystemImageSource
 
 buildscript {
     val configDir by rootProject.extra { "$rootDir/config" }
-    val projectKey: String by rootProject.extra("mobile-android-ui")
-    val projectId: String by rootProject.extra("uk.gov.android")
     val composeKotlinCompilerVersion by rootProject.extra { "1.5.0" }
-    val buildLogicDir: String by extra("mobile-android-pipelines/buildLogic")
+    val buildLogicDir: String by rootProject.extra("mobile-android-pipelines/buildLogic")
 
-    val baseNamespace by rootProject.extra { "uk.gov.android.ui" }
-
-    val localProperties = java.util.Properties()
-    if (rootProject.file("local.properties").exists()) {
-        println(localProperties)
-        localProperties.load(java.io.FileInputStream(rootProject.file("local.properties")))
-    }
-
-    fun findPackageVersion(): String {
-        var version = "1.0.0"
-
-        if (rootProject.hasProperty("packageVersion")) {
-            version = rootProject.property("packageVersion") as String
-        } else if (localProperties.getProperty("packageVersion") != null) {
-            version = localProperties.getProperty("packageVersion") as String
-        }
-
-        return version
-    }
-
-    val packageVersion by rootProject.extra { findPackageVersion() }
+    // Github packages publishing configuration
+    val githubRepositoryName: String by rootProject.extra("mobile-android-ui")
+    val mavenGroupId: String by rootProject.extra("uk.gov.android")
+    // Sonar configuration
+    val sonarProperties: Map<String, String> by rootProject.extra(
+        mapOf(
+            "sonar.projectKey" to "mobile-android-ui",
+            "sonar.projectName" to "mobile-android-ui"
+        )
+    )
 
     repositories {
         google()
@@ -37,15 +26,23 @@ buildscript {
 }
 
 val apkConfig by rootProject.extra(
-    object: ApkConfig {
+    object : ApkConfig {
         override val applicationId: String = "uk.gov.android.ui"
         override val debugVersion: String = "DEBUG_VERSION"
-        override val sdkVersions = object: ApkConfig.SdkVersions {
+        override val sdkVersions = object : ApkConfig.SdkVersions {
             override val minimum = 29
             override val target = 34
             override val compile = 34
         }
     }
+)
+
+val emulatorConfig by rootProject.extra(
+    EmulatorConfig(
+        systemImageSources = setOf(SystemImageSource.GOOGLE_ATD),
+        androidApiLevels = setOf(33),
+        deviceFilters = setOf("Pixel XL"),
+    )
 )
 
 plugins {
