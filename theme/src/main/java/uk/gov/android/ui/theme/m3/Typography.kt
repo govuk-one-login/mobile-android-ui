71790827a888e1ab57f78080027db8d72076f567
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2025-01-06 14:29:09 +0000

    fix: Label Medium
    Reinstates Typography LabelMedium as it was
    In updating Typography so that Buttons can automatically pick up the correct style (LabelLarge) I failed to check if LabelMedium was in use
    LabelMedium is used in Wallet
    
    Resolves: 9639

--- theme/src/main/java/uk/gov/android/ui/theme/m3/Typography.kt
+++ theme/src/main/java/uk/gov/android/ui/theme/m3/Typography.kt
@@ -113,6 +113,24 @@ val Typography = Typography(
         fontSize = buttonTextSize,
         lineHeight = buttonLineHeight,
     ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(
+            Font(
+                resId = R.font.gds_transport_light,
+                weight = FontWeight.Light,
+            ),
+            Font(
+                resId = R.font.gds_transport_light,
+                weight = FontWeight.Normal,
+            ),
+            Font(
+                resId = R.font.gds_transport_bold,
+                weight = FontWeight.Bold,
+            ),
+        ),
+        fontSize = buttonTextSize,
+        lineHeight = buttonLineHeight,
+    ),
 )
 
 @Preview
