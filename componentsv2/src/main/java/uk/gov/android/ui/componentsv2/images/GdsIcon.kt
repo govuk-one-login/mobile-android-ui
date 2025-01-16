b5c43737430740de3c6afe5b62339e5e99001ecf
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-01-16 15:49:13 +0000

    refactor: Remove material 2 components

--- componentsv2/src/main/java/uk/gov/android/ui/componentsv2/images/GdsIcon.kt
+++ componentsv2/src/main/java/uk/gov/android/ui/componentsv2/images/GdsIcon.kt
@@ -4,7 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
