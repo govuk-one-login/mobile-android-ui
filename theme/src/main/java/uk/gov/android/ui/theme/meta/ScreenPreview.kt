40b09f3e89d63e9a1b286b0671ebdc3e959fcb6f
Tom Ient <thomas.ient@digital.cabinet-office.gov.uk>
2025-01-27 12:04:01 +0000

    fix: DCMAW 11237 Exclude and Conflate Previews (#178)
    # _[DCMAW-11237](https://govukverify.atlassian.net/browse/DCMAW-11237):
    Exclude and Conflate Previews_
    
    -Add meta package to theme
    - Add ExcludeFromJacocoGeneratedReport an annotation hack to ignore the
    annotated method from Jacoco coverage
    - Add ScreenPreview a custom annotation to combine the commonly used
    Preview annotations (reduces clutter)
    
    ## Evidence of the change
    
    - No evidence provided
    - Write up about how it works provided in this [confluence write
    up](https://govukverify.atlassian.net/wiki/x/JoAeEgE)
    - This is already in use from this [PR for
    10391](https://github.com/govuk-one-login/mobile-android-one-login-app/pull/257)
    
    ## Checklist
    
    ### Before creating the pull request
    
    - [x] Commit messages that conform to conventional commit messages.
    - [x] Ran the app locally ensuring it builds.
    - [x] Tests pass locally.
    - [x] Pull request has a clear title with a short description about the
    feature or update.
    - [x] Created a `draft` pull request if it's not ready for review.
    
    ### Before the CODEOWNERS review the pull request
    
    - [x] Complete all Acceptance Criteria within Jira ticket.
    - [x] Self-review code.
    - [x] Successfully run changes on a testing device.
    - [x] Complete automated Testing:
      * [x] Unit Tests.
      * [x] Integration Tests.
      * [x] Instrumentation / Emulator Tests.
    - [x] Review [Accessibility considerations].
    - [x] Handle PR comments.
    
    ### Before merging the pull request
    
    - [x] [Sonar cloud report] passes inspections for your PR.
    - [x] Resolve all comments.
    
    [Sonar cloud report]:
    https://sonarcloud.io/project/overview?id=di-mobile-android-ui
    [Accessibility considerations]:
    https://developer.android.com/guide/topics/ui/accessibility/testing
    
    [DCMAW-11237]:
    https://govukverify.atlassian.net/browse/DCMAW-11237?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ theme/src/main/java/uk/gov/android/ui/theme/meta/ScreenPreview.kt
@@ -0,0 +1,10 @@
+package uk.gov.android.ui.theme.meta
+
+import androidx.compose.ui.tooling.preview.PreviewFontScale
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+
+@PreviewLightDark
+@PreviewFontScale
+@PreviewScreenSizes
+annotation class ScreenPreview
