ddf91c0054115bca55c1922c2f4da4912d26dec3
de1phine <delphine.mubiligi@digital.cabinet-office.gov.uk>
2025-02-03 15:08:56 +0000

    wip: make content vertically and horizontally aligned
    Resolves: DCMAW-10552

--- components/src/main/java/uk/gov/android/ui/patterns/GDSCentreAlignedScreen.kt
+++ components/src/main/java/uk/gov/android/ui/patterns/GDSCentreAlignedScreen.kt
@@ -1,38 +1,48 @@
 package uk.gov.android.ui.patterns
 
-import android.content.res.Configuration
+import android.text.Layout
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import uk.gov.android.ui.components.R
+import uk.gov.android.ui.theme.m3.Typography
 
 @Composable
-fun GDSCentreAlignedScreen() {
+fun GDSCentreAlignedScreen(image: Int? = null, title: String) {
     Column(
         modifier = Modifier
             .padding(start = 16.dp, top = 0.dp, end = 16.dp, bottom = 0.dp)
-            .fillMaxWidth()
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Column (
+
+        ) {
+            image?.let { painterResource(it) }
+            Text(
+                text = title,
+                style = Typography.headlineLarge,
+            )
+        }
     }
 }
 
-@Preview(
-    showBackground = true,
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-)
-
-@Preview(
-    showBackground = true,
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-)
 
 @Composable
-private fun Preview(
+@Preview
+private fun GDSCentreAlignedScreenPreview(
 ) {
     GDSCentreAlignedScreen(
+        image = R.drawable.preview__gdsvectorimage,
+        title = "Title"
     )
 }
