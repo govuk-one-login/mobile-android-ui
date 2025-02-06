9549c3e38ec1d64e89545e56aab448e9efeb71f6
de1phine <delphine.mubiligi@digital.cabinet-office.gov.uk>
2025-02-06 12:28:11 +0000

    wip: update the layout of the pattern
    - change the `padding` in the spacer to `Modifier.weight` to make
      the space between elements more flexible. Padding has also been
      added to the `Column` elements as well
    - Move the bottom content into the main content are
    - Make the content in the main area scrollable
    
    Resolves: DCMAW-10552

--- components/src/main/java/uk/gov/android/ui/patterns/GDSCentreAlignedScreen.kt
+++ components/src/main/java/uk/gov/android/ui/patterns/GDSCentreAlignedScreen.kt
@@ -10,13 +10,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -39,8 +42,6 @@ fun GDSCentreAlignedScreen(
     @StringRes primaryButtonText: Int? = null,
     @StringRes secondaryButtonText: Int? = null
 ) {
-    val context = LocalContext.current
-
     Column(
         modifier = Modifier
             .padding(
@@ -54,102 +55,108 @@ fun GDSCentreAlignedScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
     ) {
-        Spacer(
-            modifier = Modifier
-                .weight(0.2f)
-                .fillMaxWidth()
-        )
         Column(
-            modifier = Modifier.weight(0.5f),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            image?.let {
-                Image(
-                    painter = painterResource(it),
-                    contentDescription = "Optional Image"
-                )
-            }
-
-            Spacer(modifier = Modifier.padding(spacingSingle))
-
-            Text(
-                text = context.getString(title),
-                style = Typography.headlineLarge,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-
-            Spacer(modifier = Modifier.padding(spacingSingle))
-
-            body?.let {
-                body.bodyText?.forEach {
-                    Text(
-                        text = context.getString(it),
-                        style = Typography.bodyLarge,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
-                    Spacer(modifier = Modifier.padding(spacingSingle))
-                }
-
-                body.bulletList?.let {
-                    GdsBulletList(it)
-                }
-            }
-        }
-
-        Spacer(
-            Modifier
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize()
-                .weight(0.2f)
-        )
-    }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
-        modifier = Modifier
-            .fillMaxSize(),
-    ) {
-
-        Spacer(modifier = Modifier.padding(spacingSingle))
-
-        supportingText?.let {
-            Text(
-                text = context.getString(it),
-                style = Typography.bodySmall,
+                .weight(1f)
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .padding(30.dp)
+                    .fillMaxWidth()
             )
-        }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                image?.let {
+                    Image(
+                        painter = painterResource(it),
+                        contentDescription = "Optional Image"
+                    )
+                }
 
-        Spacer(modifier = Modifier.padding(spacingSingle))
+                Spacer(modifier = Modifier.padding(spacingSingle))
 
-        primaryButtonText?.let {
-            GdsButton(
-                ButtonParameters(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 32.dp, end = 32.dp),
-                    buttonType = ButtonType.PRIMARY(),
-                    onClick = {},
-                    text = context.getString(it)
+                Text(
+                    text = stringResource(title),
+                    style = Typography.headlineLarge,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
+
+                Spacer(modifier = Modifier.padding(spacingSingle))
+
+                body?.let {
+                    body.bodyText?.forEach {
+                        Text(
+                            text = stringResource(it),
+                            style = Typography.bodyLarge,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                        Spacer(modifier = Modifier.padding(spacingSingle))
+                    }
+
+                    body.bulletList?.let {
+                        GdsBulletList(it)
+                    }
+                }
+            }
+            Spacer(
+                Modifier
+                    .fillMaxSize()
+                    .padding(30.dp)
             )
         }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
 
-        Spacer(modifier = Modifier.padding(spacingSingle))
+        ) {
 
-        secondaryButtonText?.let {
-            GdsButton(
-                ButtonParameters(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 32.dp, end = 32.dp),
-                    buttonType = ButtonType.SECONDARY(),
-                    onClick = {},
-                    text = context.getString(it)
+            Spacer(modifier = Modifier.padding(spacingSingle))
+
+            supportingText?.let {
+                Text(
+                    text = stringResource(it),
+                    style = Typography.bodySmall,
                 )
-            )
-        }
+            }
 
-        Spacer(modifier = Modifier.padding(spacingSingle))
+            Spacer(modifier = Modifier.padding(spacingSingle))
+
+            primaryButtonText?.let {
+                GdsButton(
+                    ButtonParameters(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 32.dp, end = 32.dp),
+                        buttonType = ButtonType.PRIMARY(),
+                        onClick = {},
+                        text = stringResource(it)
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.padding(spacingSingle))
+
+            secondaryButtonText?.let {
+                GdsButton(
+                    ButtonParameters(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 32.dp, end = 32.dp),
+                        buttonType = ButtonType.SECONDARY(),
+                        onClick = {},
+                        text = stringResource(it)
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.padding(spacingSingle))
+        }
     }
 }
 
@@ -175,8 +182,8 @@ data class Body(
 private fun GDSCentreAlignedScreenPreview(
 ) {
     val bodyText = listOf(
-        R.string.preview__GdsInformationBanner__title,
-        R.string.preview__GdsInformationBanner__title
+        R.string.preview__GdsInformationBanner__content,
+        R.string.preview__GdsInformationBanner__content
     )
     val bulletList = BulletListParameters(
         title = R.string.preview__GdsHeading__subTitle1,
@@ -184,7 +191,13 @@ private fun GDSCentreAlignedScreenPreview(
             text = arrayOf(
                 R.string.preview__GdsContent__oneLine_0,
                 R.string.preview__GdsContent__oneLine_0,
-                R.string.preview__GdsContent__oneLine_0
+                R.string.preview__GdsContent__oneLine_0,
+                R.string.preview__GdsContent__oneLine_0,
+                R.string.preview__GdsContent__oneLine_0,
+                R.string.preview__GdsContent__oneLine_0,
+                R.string.preview__GdsContent__oneLine_0,
+                R.string.preview__GdsContent__oneLine_0,
+                R.string.preview__GdsContent__oneLine_0,
             ).toIntArray()
         )
     )
