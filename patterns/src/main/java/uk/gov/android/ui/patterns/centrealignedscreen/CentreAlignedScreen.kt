56f33e7dc85e1a06afb07e8c2ba37719a0cc9f6e
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-02-27 13:20:09 +0000

    fix: add missing comma

--- patterns/src/main/java/uk/gov/android/ui/patterns/centrealignedscreen/CentreAlignedScreen.kt
+++ patterns/src/main/java/uk/gov/android/ui/patterns/centrealignedscreen/CentreAlignedScreen.kt
@@ -228,7 +228,7 @@ private fun SupportingText(
                 .padding(
                     top = spacingDouble,
                     start = spacingDouble,
-                    end = spacingDouble
+                    end = spacingDouble,
                 ),
             textAlign = TextAlign.Center,
         )
