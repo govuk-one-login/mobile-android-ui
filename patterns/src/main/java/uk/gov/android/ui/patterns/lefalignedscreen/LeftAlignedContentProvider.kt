3bab5822837f47e06035d0213caec39b2230ee8c
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-03-07 09:09:51 +0000

    wip: add content provider

--- patterns/src/main/java/uk/gov/android/ui/patterns/lefalignedscreen/LeftAlignedContentProvider.kt
+++ patterns/src/main/java/uk/gov/android/ui/patterns/lefalignedscreen/LeftAlignedContentProvider.kt
@@ -1,20 +1,81 @@
 package uk.gov.android.ui.patterns.lefalignedscreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import kotlinx.collections.immutable.ImmutableList
+import uk.gov.android.ui.patterns.R
+import uk.gov.android.ui.theme.spacingDouble
+import uk.gov.android.ui.theme.spacingTriple
 
 internal class LeftAlignedContentProvider :
     PreviewParameterProvider<LeftAlignedContent> {
-    override val values: Sequence<LeftAlignedContent>
-        get() = sequenceOf(
-            LeftAlignedContent("Do you have a UK passport or passport with a biometric chip?"),
-            LeftAlignedContent("Title 2"),
-        )
+    private val title = "Do you have a UK passport or passport with a biometric chip?"
+    private val bodyTextShort = "All UK passports have a biometric chip"
+    private val bodyTextLong =
+        "If you have a non-UK passport, it must have a biometric chip symbol on the cover"
+    private val supportingText =
+        "Check if your passport has a biometric chip, look for the rectangular biometric chip symbol on the front cover"
+    private val warning = "You cannot use your passport if it has expired"
+
+    override val values = sequenceOf(
+        LeftAlignedContent(
+            title,
+            body = listOf(
+                Body.Text(
+                    bodyTextShort,
+                    modifier = Modifier.padding(horizontal = spacingDouble),
+                ),
+                Body.Text(
+                    bodyTextLong,
+                    modifier = Modifier.padding(horizontal = spacingDouble),
+                ),
+                Body.Warning(
+                    warning,
+                    Modifier.padding(vertical = spacingTriple),
+                ),
+                Body.Image(
+                    R.drawable.preview__gdsvectorimage,
+                    "Image description",
+                    Modifier
+                        .fillMaxWidth()
+                        .background(Color.Gray),
+                ),
+            ),
+            supportingText = supportingText,
+            primaryButton = "Primary Button",
+            secondaryButton = "Secondary Button",
+        ),
+    )
 }
 
 internal data class LeftAlignedContent(
     val title: String,
-    val body: String? = null,
-    val image: Int? = null,
-    val supporting: String? = null,
+    val body: List<Body>? = null,
+    val supportingText: String? = null,
     val primaryButton: String? = null,
+    val secondaryButton: String? = null,
 )
+
+internal sealed class Body {
+    data class Text(
+        val text: String,
+        val modifier: Modifier = Modifier,
+    ) : Body()
+
+    data class Warning(
+        val text: String,
+        val modifier: Modifier = Modifier,
+    ) : Body()
+
+    data class BulletList(val bullets: ImmutableList<String>) : Body()
+
+    data class Image(
+        val image: Int,
+        val contentDescription: String,
+        val modifier: Modifier = Modifier,
+    ) : Body()
+}
