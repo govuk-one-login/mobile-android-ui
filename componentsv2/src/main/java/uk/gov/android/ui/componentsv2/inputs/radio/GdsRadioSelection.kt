8c60f46dc9141a56a1d6ec4260a0b1a0b49d18c5
de1phine <delphine.mubiligi@digital.cabinet-office.gov.uk>
2025-03-12 09:16:19 +0000

    style: remove extra indentation
    Resolves: DCMAW-11571

--- componentsv2/src/main/java/uk/gov/android/ui/componentsv2/inputs/radio/GdsRadioSelection.kt
+++ componentsv2/src/main/java/uk/gov/android/ui/componentsv2/inputs/radio/GdsRadioSelection.kt
@@ -198,9 +198,9 @@ internal class RadioSelectionProvider : PreviewParameterProvider<RadioSelectionP
                 "option one",
                 "option two",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed " +
-                        "do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " +
-                        "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
-                        "aliquip ex ea commodo consequat",
+                    "do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " +
+                    "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
+                    "aliquip ex ea commodo consequat",
             ),
             title = RadioSelectionTitle("Example Title", TitleType.Text),
         ),
