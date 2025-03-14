11963b935f9893a3f674e908b68a8ea72e8ca852
de1phine <delphine.mubiligi@digital.cabinet-office.gov.uk>
2025-03-14 12:27:01 +0000

    test: create test for onItemSelected
    Resolves: DCMAW-11571

--- componentsv2/src/androidTest/java/uk/gov/android/ui/componentsv2/inputs/radio/GdsSelectionTest.kt
+++ componentsv2/src/androidTest/java/uk/gov/android/ui/componentsv2/inputs/radio/GdsSelectionTest.kt
@@ -1,87 +1,42 @@
 package uk.gov.android.ui.componentsv2.inputs.radio
 
-import androidx.compose.ui.semantics.SemanticsProperties
-import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import org.junit.Assert
-import org.junit.Before
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
 class GdsSelectionTest {
-    private val expectedParameterSize = 6
-    private val contentList = RadioSelectionProvider().values.toList()
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Before
-    fun setUp() {
-        Assert.assertEquals(
-            "The expected size of the provider has changed!",
-            expectedParameterSize,
-            contentList.size,
-        )
-    }
-
     @Test
-    fun testAllElementsAreDisplayed() {
-        val sampleContent = RadioSelectionProvider().values.toList()[1]
+    fun testOnItemSelectedCallbackIsCalled() {
+        val items: ImmutableList<String> = persistentListOf("Option 1", "Option 2", "Option 3")
+        val fakeCallback = FakeItemSelectedCallback()
 
         composeTestRule.setContent {
             GdsSelection(
-                radioSelectionItems = sampleContent.items,
-                title = sampleContent.title,
+                items = items,
+                selectedItem = null,
+                onItemSelected = fakeCallback::invoke
             )
         }
 
-        sampleContent.items.forEach { option ->
-            composeTestRule.onNodeWithText(option, useUnmergedTree = true).assertExists()
-        }
-
-        sampleContent.title?.let {
-            composeTestRule.onNodeWithText(it.text, useUnmergedTree = true).assertExists()
-        }
-
-        composeTestRule.onNode(
-            SemanticsMatcher.keyIsDefined(SemanticsProperties.Selected) and
-                SemanticsMatcher.expectValue(SemanticsProperties.Selected, true),
-            useUnmergedTree = true,
-        ).assertExists()
-
-        composeTestRule.onNode(
-            SemanticsMatcher.keyIsDefined(SemanticsProperties.Selected) and
-                SemanticsMatcher.expectValue(SemanticsProperties.Selected, false),
-            useUnmergedTree = true,
-        ).assertExists()
+        composeTestRule.onNodeWithText("Option 2", useUnmergedTree = true).performClick()
+        assertEquals(1, fakeCallback.itemIndex)
     }
+}
 
-    @Test
-    fun testChangeSelection() {
-        val sampleContent = RadioSelectionProvider().values.toList()[1]
+class FakeItemSelectedCallback {
+    var itemIndex: Int? = null
+        private set
 
-        composeTestRule.setContent {
-            GdsSelection(
-                radioSelectionItems = sampleContent.items,
-                title = sampleContent.title,
-            )
-        }
-
-        val secondOption = sampleContent.items[1]
-        composeTestRule.onNodeWithText(secondOption, useUnmergedTree = true).performClick()
-
-        composeTestRule.onNode(
-            SemanticsMatcher.keyIsDefined(SemanticsProperties.Selected) and
-                SemanticsMatcher.expectValue(SemanticsProperties.Selected, false),
-            useUnmergedTree = true,
-        ).assertExists()
-
-        composeTestRule.onNode(
-            SemanticsMatcher.keyIsDefined(SemanticsProperties.Selected) and
-                SemanticsMatcher.expectValue(SemanticsProperties.Selected, true),
-            useUnmergedTree = true,
-        ).assertExists()
+    fun invoke(index: Int) {
+        itemIndex = index
     }
 }
