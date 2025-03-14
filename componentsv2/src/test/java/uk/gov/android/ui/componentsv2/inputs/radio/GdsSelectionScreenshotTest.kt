f91c9330eeee769f3f77ec537678e1bbeac6bc85
de1phine <delphine.mubiligi@digital.cabinet-office.gov.uk>
2025-03-14 13:19:40 +0000

    test: add screenshot tests
    Resolves: DCMAW-11571

---
+++ componentsv2/src/test/java/uk/gov/android/ui/componentsv2/inputs/radio/GdsSelectionScreenshotTest.kt
@@ -0,0 +1,38 @@
+package uk.gov.android.ui.componentsv2.inputs.radio
+
+import androidx.compose.runtime.Composable
+import com.android.resources.NightMode
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import uk.gov.android.ui.componentsv2.BaseScreenshotTest
+
+@RunWith(Parameterized::class)
+internal class GdsSelectionScreenshotTest(
+    private val parameters: Pair<RadioSelectionPreviewData, NightMode>,
+) : BaseScreenshotTest(parameters.second) {
+
+    override val generateComposeLayout: @Composable () -> Unit = {
+        val parameters = parameters.first
+        GdsSelection(
+            items = parameters.items,
+            selectedItem = parameters.selectedIndex,
+            onItemSelected = {},
+            title = parameters.title,
+        )
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{index}GdsSelectionV2")
+        fun values(): List<Pair<RadioSelectionPreviewData, NightMode>> {
+            val result: MutableList<Pair<RadioSelectionPreviewData, NightMode>> = mutableListOf()
+
+            RadioSelectionProvider().values.forEach { previewData ->
+                NightMode.entries.forEach { nightMode ->
+                    result.add(Pair(previewData, nightMode))
+                }
+            }
+            return result
+        }
+    }
+}
