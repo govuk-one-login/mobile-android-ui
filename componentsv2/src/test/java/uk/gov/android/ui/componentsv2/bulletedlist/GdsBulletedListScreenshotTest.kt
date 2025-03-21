c1b6c0c41d62a81ad96ce4cb4471d1e9b280fabe
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-03-21 09:42:03 +0000

    style: Changing file name only for `GdsBulletedList`
    - changing file name to alisgn with the Composable name and the
    Design system one
    - shouldn't create any breaking change as the name of the composable
    name was correct

--- componentsv2/src/test/java/uk/gov/android/ui/componentsv2/bulletedlist/GdsBulletedListScreenshotTest.kt
+++ componentsv2/src/test/java/uk/gov/android/ui/componentsv2/bulletedlist/GdsBulletedListScreenshotTest.kt
@@ -7,7 +7,7 @@ import org.junit.runners.Parameterized
 import uk.gov.android.ui.componentsv2.BaseScreenshotTest
 
 @RunWith(Parameterized::class)
-internal class BulletedListScreenshotTest(
+internal class GdsBulletedListScreenshotTest(
     private val parameters: Pair<BulletedListItem, NightMode>,
 ) : BaseScreenshotTest(parameters.second) {
     override val generateComposeLayout: @Composable () -> Unit = {
