aa343456265386668b81768b30a6b86d17be189a
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-03-28 12:47:58 +0000

    feat: DCMAW-8796 | allow configuration of GdsHeading

--- componentsv2/src/main/java/uk/gov/android/ui/componentsv2/heading/GdsHeading.kt
+++ componentsv2/src/main/java/uk/gov/android/ui/componentsv2/heading/GdsHeading.kt
@@ -8,6 +8,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -22,6 +24,8 @@ import uk.gov.android.ui.theme.util.UnstableDesignSystemAPI
 fun GdsHeading(
     text: String,
     modifier: Modifier = Modifier,
+    style: TextStyle = Typography.displaySmall,
+    fontWeight: FontWeight? = null,
     textAlign: TextAlign = TextAlign.Start,
 ) {
     val heading = stringResource(R.string.heading, text)
@@ -29,7 +33,8 @@ fun GdsHeading(
     Text(
         text = text,
         color = MaterialTheme.colorScheme.onBackground,
-        style = Typography.displaySmall,
+        style = style,
+        fontWeight = fontWeight,
         modifier = modifier
             .fillMaxWidth()
             .semantics { contentDescription = heading },
@@ -37,10 +42,17 @@ fun GdsHeading(
     )
 }
 
-internal class HeadingParameterPreviewProvider : PreviewParameterProvider<String> {
-    override val values: Sequence<String> = sequenceOf(
-        "Short Title",
-        "Long Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+internal data class HeadingParameters(
+    val text: String,
+    val style: TextStyle = Typography.displaySmall,
+    val fontWeight: FontWeight? = null,
+)
+
+internal class HeadingParameterPreviewProvider : PreviewParameterProvider<HeadingParameters> {
+    override val values: Sequence<HeadingParameters> = sequenceOf(
+        HeadingParameters("Short Title"),
+        HeadingParameters("Long Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+        HeadingParameters("Subtitle", style = Typography.bodySmall, fontWeight = FontWeight.W700),
     )
 }
 
