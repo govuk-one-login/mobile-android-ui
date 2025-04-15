cd89aced19ae0d2188f97fd33224291a07fdf30a
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2025-04-15 17:57:48 +0100

    fix: Detekt fail

--- componentsv2/src/main/java/uk/gov/android/ui/componentsv2/numberedlist/GdsNumberedList.kt
+++ componentsv2/src/main/java/uk/gov/android/ui/componentsv2/numberedlist/GdsNumberedList.kt
@@ -238,7 +238,10 @@ internal class NumberedListProvider : PreviewParameterProvider<BulletedListItem>
             items = persistentListOf(
                 "First line",
                 "Second line",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
+                    "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+                    "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat",
             ),
         ),
         BulletedListItem(
