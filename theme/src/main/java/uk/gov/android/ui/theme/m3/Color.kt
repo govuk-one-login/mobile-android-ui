19efcb22dab0cf63031b486227254ce0d5c06962
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-06-09 16:28:28 +0100

    feat: Amend colors as per design requirements

--- theme/src/main/java/uk/gov/android/ui/theme/m3/Color.kt
+++ theme/src/main/java/uk/gov/android/ui/theme/m3/Color.kt
@@ -5,6 +5,102 @@ import androidx.compose.ui.graphics.Color
 // Admin button
 val adminButton = Color(0xFF4C2C92)
 
+object Backgrounds {
+    val screen = ColorPair(Color(0xFFFFFFFF), Color(0xFF0B0C0C))
+    val card = ColorPair(Color(0xFFF3F2F1), Color(0xFF262626))
+    val list = ColorPair(Color(0xFFF3F2F1), Color(0xFF262626))
+    val topBar = ColorPair(Color(0xFFFFFFFF), Color(0xFF0B0C0C))
+    val topBarScrolled = ColorPair(Color(0xFFF3F2F1), Color(0xFF262626))
+    val navigationBar = ColorPair(Color(0xFFFFFFFF), Color(0xFF262626))
+    val validWallet = ColorPair(Color(0xFFFFFFFF))
+    val expiredWallet = ColorPair(Color(0xFFF3F2F1))
+    val walletDocumentHeader = ColorPair(Color(0xFFFFFFFF))
+    val homeHeader = ColorPair(Color(0xFF1D70B8), Color(0xFF0F385C))
+    val veteranCardInformationHeader = ColorPair(Color(0xFF532A45), Color(0xFF532A45))
+}
+
+object Buttons {
+    val primary = ColorPair(Color(0xFF00703C), Color(0xFF008547))
+    val primaryIconAndText = ColorPair(Color(0xFFFFFFFF), Color(0xFFFFFFFF))
+    val primaryFocusState = ColorPair(Color(0xFFFFDD00), Color(0xFFFFDD00))
+    val primaryTextAndIconInFocusState = ColorPair(light = Color(0xFF0B0C0C))
+    val secondaryTextAndIcon = ColorPair(light = Color(0xFF00703C), dark = Color(0xFF03CD6E))
+    val disableText = ColorPair(light = Color(0xFFB1B4B6))
+    val disabledTextAndIcon = Color(0xFFFFFFFF)
+    val destructive = ColorPair(Color(0xFFD4351C))
+    val destructiveTextAndIcon = ColorPair(Color(0xFFFFFFFF))
+    val nativeButtonText = ColorPair(Color(0xFF00703C), Color(0xFF03CD6E))
+    val destructiveNativeButtonText = ColorPair(Color(0xFFD4351C), Color(0xFFFF6961))
+    val icon = ColorPair(Color(0xFF00703C), Color(0xFF03CD6E))
+}
+
+object Dividers {
+    val card = ColorPair(Color(0xFF505A5f), Color(0xFFB1B4B6))
+    val documentInformation = ColorPair(Color(0xFF6F777B), Color(0xFFFFFFFF))
+}
+
+object Borders {
+    val walletDocument = ColorPair(Color(0xFFB1B4B6))
+    val walletDocumentHeader = ColorPair(Color(0x0DFFFFFF), Color(0x0D000000))
+    val statusTag = ColorPair(Color(0xFFD4351C))
+    val liveDocumentFooter = ColorPair(Color(0x33FFFFFF), Color(0x33000000))
+}
+
+object Icons {
+    val icon = ColorPair(Color(0xFF0B0C0C), Color(0xFFFFFFFF))
+    val success = ColorPair(Color(0xFF00703C), Color(0xFF008547))
+    val error = ColorPair(Color(0xFF0B0C0C), Color(0xFFFFFFFF))
+    val destructive = ColorPair(Color(0xFFD4351C))
+    val spinner = ColorPair(Color(0xFF00703C), Color(0xFF03CD6E))
+}
+
+object Links {
+    val link = ColorPair(light = Color(0xFF00703C), dark = Color(0xFF03CD6E))
+}
+
+object Text {
+    val primary = ColorPair(Color(0xFF0B0C0C), Color(0xFFFFFFFF))
+    val secondary = ColorPair(Color(0xFF505A5F), Color(0xFFFFFFFF))
+    val statusTag = ColorPair(Color(0xFFD4351C))
+    val validWalletDocumentAction = ColorPair(Color(0xFF00703C))
+    val invalidWalletDocumentAction = ColorPair(Color(0xFFD4351C))
+    val walletDocument = ColorPair(Color(0xFF0B0C0C))
+    val liveDocumentFooter = ColorPair(Color(0xFF0B0C0C))
+    val liveDocumentHeader = ColorPair(Color(0xFFFFFFFF))
+}
+
+object Shadows {
+    val walletDocument = ColorPair(Color(0x1F000000))
+}
+
+object NavigationElements {
+    val topBarTitle = ColorPair(Color(0xFF0B0C0C), Color(0xFFFFFFFF))
+    val topBarIcon = ColorPair(Color(0xFF00703C), Color(0xFF03CD6E))
+    val navigationBarIconAndLabel = ColorPair(Color(0xFF0B0C0C), Color(0xFFFFFFFF))
+    val navigationBarSelectedState = ColorPair(Color(0xFFCCE2DB), Color(0xFF008547))
+}
+
+object Selection {
+    val unselectedRadioButton = ColorPair(Color(0xFF49454F), Color(0xFFFFFFFF))
+    val selectedRadioButton = ColorPair(Color(0xFF00703C), Color(0xFF03CD6E))
+}
+
+object Switch {
+    val unselectedBackground = ColorPair(Color(0xFFF3F2F1), Color(0xFF262626))
+    val unselectedBorderAndHandle = ColorPair(Color(0xFF0B0C0C), Color(0xFF000000))
+    val selectedBackground = ColorPair(Color(0xFF00703C), Color(0xFF03CD6E))
+    val selectedHandle = ColorPair(Color(0xFFFFFFFF))
+}
+
+/**
+ * Class that allows setting the corresponding dark and light mode colors.
+ *
+ * When the colors are the same for both environments, then  the dark mode can be left as null as it will default to the light color.
+ */
+data class ColorPair(
+    val light: Color,
+    var dark: Color = light,
+)
 // M3 Colors (all options) - the ones using hex are mapped to the GDS Design System,
 // the compose provided colors are not specified/ used
 
