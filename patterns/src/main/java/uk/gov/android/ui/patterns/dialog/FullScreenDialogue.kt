ca199e8b7a9a6264ca3ffbed805ccb3919d796ef
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2025-07-17 14:32:18 +0100

    feat: Add missing change

--- patterns/src/main/java/uk/gov/android/ui/patterns/dialog/FullScreenDialogue.kt
+++ patterns/src/main/java/uk/gov/android/ui/patterns/dialog/FullScreenDialogue.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
