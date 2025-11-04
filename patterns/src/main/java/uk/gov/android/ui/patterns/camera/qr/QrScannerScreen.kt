ded0c94442fc7f568057a8701cde6ae3a4b6612e
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2025-11-04 13:49:25 +0000

    feat: Create `QrScannerScreen` composable
    - The `QrScannerScreen` composable now sits within the patterns gradle
      submodule.
    - Add a string resource for the instructions used in the
      QrScannerScreen. This defers to another string resource for
      reusability.

---
+++ patterns/src/main/java/uk/gov/android/ui/patterns/camera/qr/QrScannerScreen.kt
@@ -0,0 +1,157 @@
+package uk.gov.android.ui.patterns.camera.qr
+
+import android.Manifest
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.zIndex
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.rememberPermissionState
+import kotlinx.coroutines.CoroutineScope
+import uk.gov.android.ui.componentsv2.camera.CameraContent
+import uk.gov.android.ui.componentsv2.camera.CameraContentViewModel
+import uk.gov.android.ui.componentsv2.camera.CameraUseCaseProvider
+import uk.gov.android.ui.componentsv2.camera.CameraUseCaseProvider.Companion.preview
+import uk.gov.android.ui.componentsv2.camera.qr.BarcodeScanResult
+import uk.gov.android.ui.componentsv2.camera.qr.BarcodeUseCaseProviders.barcodeAnalysis
+import uk.gov.android.ui.componentsv2.camera.qr.BarcodeUseCaseProviders.provideQrScanningOptions
+import uk.gov.android.ui.componentsv2.camera.qr.BarcodeUseCaseProviders.provideZoomOptions
+import uk.gov.android.ui.componentsv2.camera.qr.CentrallyCroppedImageProxyConverter
+import uk.gov.android.ui.componentsv2.permission.PermissionLogic
+import uk.gov.android.ui.componentsv2.permission.PermissionScreen
+import uk.gov.android.ui.patterns.R
+import uk.gov.android.ui.patterns.camera.qr.ModifierExtensions.CANVAS_WIDTH_MULTIPLIER
+import uk.gov.android.ui.patterns.camera.qr.ModifierExtensions.qrScannerOverlay
+import uk.gov.android.ui.theme.m3.CustomColorsScheme
+import uk.gov.android.ui.theme.m3.GdsLocalColorScheme
+import uk.gov.android.ui.theme.smallPadding
+import uk.gov.android.ui.theme.spacingDouble
+
+@Composable
+@OptIn(ExperimentalPermissionsApi::class)
+fun QrScannerScreen(
+    barcodeAnalysisCallback: BarcodeScanResult.Callback,
+    viewModel: CameraContentViewModel,
+    modifier: Modifier = Modifier,
+    instructionText: String = stringResource(R.string.qr_scan_screen_title),
+    onPermanentCameraDenial: @Composable ((PermissionState) -> Unit) = { _ -> },
+    onShowRationale: @Composable ((PermissionState, () -> Unit) -> Unit) = { _, _ -> },
+    onRequirePermission: @Composable ((PermissionState, () -> Unit) -> Unit) = { _, _ -> },
+    scanningWidthMultiplier: Float = CANVAS_WIDTH_MULTIPLIER,
+    colorScheme: CustomColorsScheme = GdsLocalColorScheme.current,
+    context: Context = LocalContext.current,
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+) {
+    val (
+        hasPreviouslyDeniedPermission,
+        onUpdatePreviouslyDeniedPermission,
+    ) = remember { mutableStateOf(false) }
+
+    val permissionState = rememberPermissionState(Manifest.permission.CAMERA) {
+        onUpdatePreviouslyDeniedPermission(!it)
+    }
+
+    viewModel.removeUseCases()
+
+    listOf(
+        preview(viewModel::update),
+        barcodeAnalysis(
+            context = context,
+            options = provideQrScanningOptions(
+                provideZoomOptions(viewModel::getCurrentCamera),
+            ),
+            converter = CentrallyCroppedImageProxyConverter(
+                relativeScanningWidth = scanningWidthMultiplier,
+            ),
+            callback = barcodeAnalysisCallback,
+        ),
+    ).map(CameraUseCaseProvider::provide)
+        .let(viewModel::addAll)
+
+    val permissionLogic = PermissionLogic(
+        onGrantPermission = {
+            QrScannerPermissionGranted(
+                instructionText,
+                colorScheme,
+                coroutineScope,
+                viewModel,
+                scanningWidthMultiplier,
+            )
+        },
+        onPermissionPermanentlyDenied = onPermanentCameraDenial,
+        onShowRationale = onShowRationale,
+        onRequirePermission = onRequirePermission,
+    )
+
+    Column(
+        modifier = modifier
+            .background(colorScheme.dialogBackground)
+            .padding(spacingDouble)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        PermissionScreen(
+            permissionState = permissionState,
+            hasPreviouslyDeniedPermission = hasPreviouslyDeniedPermission,
+            logic = permissionLogic,
+        )
+    }
+}
+
+@Composable
+private fun QrScannerPermissionGranted(
+    instructionText: String,
+    colorScheme: CustomColorsScheme,
+    coroutineScope: CoroutineScope,
+    viewModel: CameraContentViewModel,
+    scanningWidthMultiplier: Float,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Box(
+            modifier =
+            Modifier
+                .fillMaxSize()
+                .zIndex(2f),
+            contentAlignment = Alignment.TopCenter,
+        ) {
+            Text(
+                text = instructionText,
+                style = MaterialTheme.typography.headlineMedium,
+                color = colorScheme.qrScannerOverlayBorder,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(all = smallPadding),
+            )
+        }
+        CameraContent(
+            coroutineScope = coroutineScope,
+            viewModel = viewModel,
+            modifier = Modifier
+                .fillMaxSize()
+                .testTag("cameraViewfinder")
+                .qrScannerOverlay(
+                    canvasWidthMultiplier = scanningWidthMultiplier,
+                    overlayTint = colorScheme.qrScannerOverlayBackground,
+                    qrBorderColor = colorScheme.qrScannerOverlayBorder,
+                ),
+        )
+    }
+}
--- testwrapper/src/main/java/uk/gov/android/ui/testwrapper/componentsv2/camera/qr/QrScannerScreenDemo.kt
+++ testwrapper/src/main/java/uk/gov/android/ui/testwrapper/componentsv2/camera/qr/QrScannerScreenDemo.kt
@@ -1,83 +1,28 @@
 package uk.gov.android.ui.testwrapper.componentsv2.camera.qr
 
-import android.Manifest
 import android.content.Context
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.testTag
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
-import com.google.accompanist.permissions.rememberPermissionState
-import kotlinx.coroutines.CoroutineScope
-import uk.gov.android.ui.componentsv2.camera.CameraContent
 import uk.gov.android.ui.componentsv2.camera.CameraContentViewModel
-import uk.gov.android.ui.componentsv2.camera.CameraUseCaseProvider
-import uk.gov.android.ui.componentsv2.camera.CameraUseCaseProvider.Companion.preview
-import uk.gov.android.ui.componentsv2.camera.qr.BarcodeUseCaseProviders.barcodeAnalysis
-import uk.gov.android.ui.componentsv2.camera.qr.BarcodeUseCaseProviders.provideQrScanningOptions
-import uk.gov.android.ui.componentsv2.camera.qr.BarcodeUseCaseProviders.provideZoomOptions
-import uk.gov.android.ui.componentsv2.camera.qr.CentrallyCroppedImageProxyConverter
-import uk.gov.android.ui.componentsv2.permission.PermissionLogic
-import uk.gov.android.ui.componentsv2.permission.PermissionScreen
-import uk.gov.android.ui.patterns.camera.qr.ModifierExtensions.CANVAS_WIDTH_MULTIPLIER
-import uk.gov.android.ui.patterns.camera.qr.ModifierExtensions.qrScannerOverlay
+import uk.gov.android.ui.patterns.camera.qr.QrScannerScreen
 import uk.gov.android.ui.testwrapper.componentsv2.camera.BarcodeScanResultLoggingCallback
 import uk.gov.android.ui.testwrapper.componentsv2.camera.CameraContentDemoButtons.CameraPermissionRationaleButton
 import uk.gov.android.ui.testwrapper.componentsv2.camera.CameraContentDemoButtons.CameraRequirePermissionButton
 import uk.gov.android.ui.testwrapper.componentsv2.camera.CameraContentDemoButtons.PermanentCameraDenial
-import uk.gov.android.ui.theme.m3.CustomColorsScheme
-import uk.gov.android.ui.theme.m3.GdsLocalColorScheme
-import uk.gov.android.ui.theme.spacingDouble
 
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun QrScannerScreenDemo(
     modifier: Modifier = Modifier,
-    scanningWidthMultiplier: Float = CANVAS_WIDTH_MULTIPLIER,
-    colorScheme: CustomColorsScheme = GdsLocalColorScheme.current,
     context: Context = LocalContext.current,
-    coroutineScope: CoroutineScope = rememberCoroutineScope(),
     viewModel: CameraContentViewModel = viewModel<CameraContentViewModel>(),
 ) {
-    val (
-        hasPreviouslyDeniedPermission,
-        onUpdatePreviouslyDeniedPermission,
-    ) = remember { mutableStateOf(false) }
-
-    val permissionState = rememberPermissionState(Manifest.permission.CAMERA) {
-        onUpdatePreviouslyDeniedPermission(!it)
-    }
-
-    viewModel.removeUseCases()
-
-    listOf(
-        preview(viewModel::update),
-        barcodeAnalysis(
-            context = context,
-            options = provideQrScanningOptions(
-                provideZoomOptions(viewModel::getCurrentCamera)
-            ),
-            converter = CentrallyCroppedImageProxyConverter(
-                relativeScanningWidth = scanningWidthMultiplier,
-            ),
-            callback = BarcodeScanResultLoggingCallback,
-        )
-    ).map(CameraUseCaseProvider::provide)
-        .let(viewModel::addAll)
-
     val onPermanentCameraDenial: @Composable (permissionState: PermissionState) -> Unit = { state ->
         PermanentCameraDenial(state, context)
     }
@@ -94,38 +39,12 @@ fun QrScannerScreenDemo(
         CameraRequirePermissionButton(launchPermission = launchPermission)
     }
 
-    val permissionLogic = PermissionLogic(
-        onGrantPermission = {
-            CameraContent(
-                coroutineScope = coroutineScope,
-                viewModel = viewModel,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .testTag("cameraViewfinder")
-                    .qrScannerOverlay(
-                        canvasWidthMultiplier = scanningWidthMultiplier,
-                        overlayTint = colorScheme.qrScannerOverlayBackground,
-                        qrBorderColor = colorScheme.qrScannerOverlayBorder,
-                    )
-            )
-        },
-        onPermissionPermanentlyDenied = onPermanentCameraDenial,
+    QrScannerScreen(
+        barcodeAnalysisCallback = BarcodeScanResultLoggingCallback,
+        viewModel = viewModel,
+        onPermanentCameraDenial = onPermanentCameraDenial,
         onShowRationale = onShowRationale,
         onRequirePermission = onRequirePermission,
+        modifier = modifier,
     )
-
-    Column(
-        modifier = modifier
-            .background(colorScheme.dialogBackground)
-            .padding(spacingDouble)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        PermissionScreen(
-            permissionState = permissionState,
-            hasPreviouslyDeniedPermission = hasPreviouslyDeniedPermission,
-            logic = permissionLogic,
-        )
-    }
 }
