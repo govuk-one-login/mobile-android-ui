cb332fdabf4b3977962e111a8a7972f15f002b82
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2025-11-13 10:06:44 +0000

    feat!: Remove Providers.preview
    - This has been removed due to the CameraContentViewModel handling
      the creation of a `Preview` use case internally.
    - Add KDoc to CameraUseCaseProviders.

--- componentsv2/componentsv2-camera/src/main/java/uk/gov/android/ui/componentsv2/camera/CameraUseCaseProviders.kt
+++ componentsv2/componentsv2-camera/src/main/java/uk/gov/android/ui/componentsv2/camera/CameraUseCaseProviders.kt
@@ -1,17 +1,37 @@
 package uk.gov.android.ui.componentsv2.camera
 
 import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
+import androidx.camera.core.UseCase
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import java.util.concurrent.Executor
 
+/**
+ * Companion object for holding functions that generate various camera
+ * [UseCase] objects.
+ */
 object CameraUseCaseProviders {
-    @JvmStatic
-    fun preview(
-        surfaceProvider: Preview.SurfaceProvider,
-    ): Preview = Preview.Builder().build().apply {
-        this.surfaceProvider = surfaceProvider
-    }
 
+    /**
+     * Creates an [ImageAnalysis] object that's passed to a
+     * [ProcessCameraProvider.bindToLifecycle] call.
+     *
+     * @param analyzer The behaviour to perform on each camera frame.
+     * @param backpressureStrategy The [ImageAnalysis] strategy to utilise. This changes camera
+     * frame queueing behaviour. Defaults to [ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST].
+     * @param executor The [Executor] that the provided [analyzer] runs within.
+     * @param outputImageFormat The [ImageAnalysis] output that the [ImageAnalysis.Analyzer]'s
+     * [androidx.camera.core.ImageProxy] should have. Defaults to
+     * [ImageAnalysis.OUTPUT_IMAGE_FORMAT_YUV_420_888].
+     *
+     * @see ContextCompat.getMainExecutor
+     * @see ImageAnalysis.Builder.setBackpressureStrategy
+     * @see ImageAnalysis.Builder.setOutputImageFormat
+     * @see ImageAnalysis.setAnalyzer
+     * @see ProcessCameraProvider.bindToLifecycle
+     *
+     * @return An [ImageAnalysis] object that defers to the provided [analyzer] parameter.
+     */
     @JvmStatic
     fun imageAnalysis(
         analyzer: ImageAnalysis.Analyzer,
