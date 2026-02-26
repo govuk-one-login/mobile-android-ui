e5263c77ab24ba20e8d8a3a12319fcf1b0f7b533
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2026-02-26 12:27:51 +0000

    feat: Add semantic test

--- patterns/src/androidTest/java/uk/gov/android/ui/patterns/leftalignedscreen/LeftAlignedScreenV2Test.kt
+++ patterns/src/androidTest/java/uk/gov/android/ui/patterns/leftalignedscreen/LeftAlignedScreenV2Test.kt
@@ -1,6 +1,8 @@
 package uk.gov.android.ui.patterns.leftalignedscreen
 
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertContentDescriptionEquals
@@ -11,11 +13,15 @@ import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performKeyInput
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import uk.gov.android.ui.componentsv2.list.ListItem
 
 class LeftAlignedScreenV2Test {
     @get:Rule
@@ -95,5 +101,54 @@ class LeftAlignedScreenV2Test {
             .onNode(title)
             .assertContentDescriptionEquals(titleText)
             .assert(SemanticsMatcher.expectValue(SemanticsProperties.Heading, Unit))
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun scrollableContent() {
+        composeTestRule.setContent {
+            LeftAlignedScreenV2(
+                title = titleText,
+                body = persistentListOf(
+                    LeftAlignedScreenBodyV2.BulletList(
+                        items = longListItems()
+                    )
+                )
+            )
+        }
+
+        composeTestRule
+            .onNode(hasText("Item one"))
+            .performKeyInput { keyDown(Key.DirectionDown) }
+            .performKeyInput { keyDown(Key.DirectionUp) }
+    }
+
+    private fun longListItems(): PersistentList<ListItem> {
+        return persistentListOf(
+            ListItem(text = "Item one"),
+            ListItem(text = "Item two"),
+            ListItem(text = "Item three"),
+            ListItem(text = "Item four"),
+            ListItem(text = "Item five"),
+            ListItem(text = "Item six"),
+            ListItem(text = "Item seven"),
+            ListItem(text = "Item eight"),
+            ListItem(text = "Item nine"),
+            ListItem(text = "Item ten"),
+            ListItem(text = "Item eleven"),
+            ListItem(text = "Item twelve"),
+            ListItem(text = "Item thirteen"),
+            ListItem(text = "Item fourteen"),
+            ListItem(text = "Item fifteen"),
+            ListItem(text = "Item sixteen"),
+            ListItem(text = "Item seventeen"),
+            ListItem(text = "Item eighteen"),
+            ListItem(text = "Item nineteen"),
+            ListItem(text = "Item twenty"),
+            ListItem(text = "Item twenty one"),
+            ListItem(text = "Item twenty two"),
+            ListItem(text = "Item twenty three"),
+            ListItem(text = "Item twenty four"),
+        )
     }
 }
