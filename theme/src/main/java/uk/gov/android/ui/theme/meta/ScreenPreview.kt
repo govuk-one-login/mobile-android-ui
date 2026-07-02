523493a7773fcaf60ddacbbe14b042ea7aee7942
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-07-02 11:03:20 +0100

    refactor: Rename preview annotation according to compose linter

--- theme/src/main/java/uk/gov/android/ui/theme/meta/ScreenPreview.kt
+++ theme/src/main/java/uk/gov/android/ui/theme/meta/ScreenPreview.kt
@@ -4,6 +4,14 @@ import androidx.compose.ui.tooling.preview.PreviewFontScale
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 
+@Deprecated(
+    message = "Use ScreenPreviews instead. Planned for removal on 2 October 2026.",
+    replaceWith = ReplaceWith(
+        "ScreenPreviews",
+        "uk.gov.android.ui.theme.meta.ScreenPreviews",
+    ),
+)
+@Suppress("ComposePreviewNaming")
 @PreviewLightDark
 @PreviewFontScale
 @PreviewScreenSizes
