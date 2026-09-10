abe22462b08d4dc2c9ff52875ead48d4d97076be
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-10 12:45:37 +0100

    fix: Fix text alignment

--- patterns/src/test/java/uk/gov/android/ui/patterns/loadingscreen/v2/LoadingScreenshotTest.kt
+++ patterns/src/test/java/uk/gov/android/ui/patterns/loadingscreen/v2/LoadingScreenshotTest.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.Composable
 import com.android.resources.NightMode
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
+import uk.gov.android.ui.componentsv2.progress.GdsProgressIndicator
+import uk.gov.android.ui.componentsv2.progress.GdsProgressIndicatorDefaults
+import uk.gov.android.ui.componentsv2.progress.ProgressLabels
 import uk.gov.android.ui.patterns.BaseScreenshotTest
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.android.ui.theme.util.UnstableDesignSystemAPI
@@ -14,7 +17,13 @@ class LoadingScreenshotTest(nightMode: NightMode) : BaseScreenshotTest(nightMode
 
     override val generateComposeLayout: @Composable () -> Unit = {
         GdsTheme {
-            LoadingScreen()
+            LoadingScreen {
+                GdsProgressIndicator(
+                    labels = GdsProgressIndicatorDefaults.labels(
+                        short = "Custom long loading message that will wrap onto multiple lines",
+                    ),
+                )
+            }
         }
     }
 
