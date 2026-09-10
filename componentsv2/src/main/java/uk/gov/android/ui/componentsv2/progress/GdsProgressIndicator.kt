a35a8e032b13e5e4ebd49ce2352d245259ffc24c
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-10 12:45:37 +0100

    fix: Fix text alignment

--- componentsv2/src/main/java/uk/gov/android/ui/componentsv2/progress/GdsProgressIndicator.kt
+++ componentsv2/src/main/java/uk/gov/android/ui/componentsv2/progress/GdsProgressIndicator.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import uk.gov.android.ui.theme.m3.GdsLocalColorScheme
@@ -46,7 +47,10 @@ fun GdsProgressIndicator(
         CircularProgressIndicator(
             color = GdsLocalColorScheme.current.spinnerIcon,
         )
-        Text(text = labels.get(state.waitedFor))
+        Text(
+            text = labels.get(state.waitedFor),
+            textAlign = TextAlign.Center,
+        )
     }
 }
 
