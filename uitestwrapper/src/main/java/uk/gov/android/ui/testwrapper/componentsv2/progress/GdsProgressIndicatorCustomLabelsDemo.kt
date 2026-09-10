30dccbeb38392e48fe03cfedccf21ce59c267b69
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-10 12:24:47 +0100

    feat: Add Loading Screen v2

--- uitestwrapper/src/main/java/uk/gov/android/ui/testwrapper/componentsv2/progress/GdsProgressIndicatorCustomLabelsDemo.kt
+++ uitestwrapper/src/main/java/uk/gov/android/ui/testwrapper/componentsv2/progress/GdsProgressIndicatorCustomLabelsDemo.kt
@@ -6,26 +6,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import uk.gov.android.ui.componentsv2.progress.GdsProgressIndicator
 import uk.gov.android.ui.componentsv2.progress.GdsProgressIndicatorDefaults
-import uk.gov.android.ui.patterns.leftalignedscreen.LeftAlignedScreen
+import uk.gov.android.ui.patterns.loadingscreen.v2.LoadingScreen
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.android.ui.theme.meta.ContentPreviews
 
 @Composable
-fun GdsProgressIndicatorCustomLabelsDemo(modifier: Modifier = Modifier) = LeftAlignedScreen(
+fun GdsProgressIndicatorCustomLabelsDemo(modifier: Modifier = Modifier) = LoadingScreen(
     modifier = modifier,
-    body = { horizontalPadding ->
-        item {
-            GdsProgressIndicator(
-                labels = GdsProgressIndicatorDefaults.labels(
-                    short = "Custom loading message that is quite long and might spill over",
-                    long = "Custom long loading message that is very long and should overflow",
-                    longer = "Custom longer loading message that is just as long and might wrap",
-                ),
-                modifier = Modifier.padding(horizontal = horizontalPadding),
-            )
-        }
-    },
-)
+) {
+    GdsProgressIndicator(
+        labels = GdsProgressIndicatorDefaults.labels(
+            short = "Custom loading message that is quite long and might spill over",
+            long = "Custom long loading message that is very long and should overflow",
+            longer = "Custom longer loading message that is just as long and might wrap",
+        ),
+    )
+}
 
 @Composable
 @PreviewLightDark
