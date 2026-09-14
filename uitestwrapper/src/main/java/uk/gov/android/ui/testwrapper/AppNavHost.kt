403ee6a35580044dfa1623696abbaba9fa22eb88
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-14 15:46:43 +0100

    fix: Fix layout of demo app tab menu

--- uitestwrapper/src/main/java/uk/gov/android/ui/testwrapper/AppNavHost.kt
+++ uitestwrapper/src/main/java/uk/gov/android/ui/testwrapper/AppNavHost.kt
@@ -1,13 +1,9 @@
 package uk.gov.android.ui.testwrapper
 
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import kotlinx.coroutines.launch
@@ -22,30 +18,24 @@ fun AppNavHost(
     startDestination: Any,
     modifier: Modifier = Modifier,
 ) {
-    val tabPagesOffsetPadding = 50.dp
     val scope = rememberCoroutineScope()
     NavHost(
         navController,
         modifier = modifier,
         startDestination = startDestination,
     ) {
-        val mod =
-            Modifier
-                .statusBarsPadding()
-                .navigationBarsPadding()
-                .padding(top = tabPagesOffsetPadding)
-                .testTag("entries")
+        val modifier = Modifier.testTag("entries")
 
-        applyTabDestinations(modifier = mod, onNavigate = navController::navigate)
+        applyTabDestinations(modifier = modifier, onNavigate = navController::navigate)
         applyComponentDestinations(
-            modifier = mod,
+            modifier = modifier,
             onNavigate = {
                 scope.launch {
                     navController.navigate(it)
                 }
             },
         )
-        applyPatternDestinations(modifier = mod)
-        applyThemeDestinations(modifier = mod)
+        applyPatternDestinations(modifier = modifier)
+        applyThemeDestinations(modifier = modifier)
     }
 }
