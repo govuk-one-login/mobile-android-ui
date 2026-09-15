4c5f2bb1d6bc53defbb72710069a10042d9bb4a4
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-09-15 11:40:42 +0100

    feat: update radio button behaviour in focus state
    -  remove keyEvent listener workaround
    
    Resolves: DCMAW-15592

--- componentsv2/src/main/java/uk/gov/android/ui/componentsv2/inputs/radio/GdsRadios.kt
+++ componentsv2/src/main/java/uk/gov/android/ui/componentsv2/inputs/radio/GdsRadios.kt
@@ -30,11 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.input.key.type
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.contentDescription
@@ -137,16 +132,6 @@ internal fun GdsRadioOptionItem(
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
-            .onKeyEvent {
-                if (it.type == KeyEventType.KeyUp &&
-                    (it.key == Key.Spacebar || it.key == Key.Enter)
-                ) {
-                    onOptionSelected()
-                    true
-                } else {
-                    false
-                }
-            }
             .selectable(
                 selected = isSelected,
                 onClick = onOptionSelected,
