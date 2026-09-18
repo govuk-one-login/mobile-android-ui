793b761f459e7bf680220e6c4d047afae4adfaed
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-18 09:21:02 +0100

    fix: Suggest annotated string preview improvements

--- componentsv2/src/main/java/uk/gov/android/ui/componentsv2/text/annotatedstringparameters/GdsAnnotatedStringPreviewData.kt
+++ componentsv2/src/main/java/uk/gov/android/ui/componentsv2/text/annotatedstringparameters/GdsAnnotatedStringPreviewData.kt
@@ -6,16 +6,15 @@ import androidx.compose.ui.text.font.FontWeight
 import uk.gov.android.ui.componentsv2.R
 import uk.gov.android.ui.theme.m3.Typography
 
-data class GdsAnnotatedStringPreviewData(
-    val text: Int,
-    val fontWeight: FontWeight,
-    val icon: Int,
+internal data class GdsAnnotatedStringPreviewData(
+    val text: String = "GdsAnnotatedString",
+    val fontWeight: FontWeight = FontWeight.Normal,
+    val icon: Int = R.drawable.ic_external_site,
     val iconId: Int = R.string.in_line_icon_id,
     val textStyle: TextStyle = Typography.labelLarge,
-    val iconContentDescription: Int,
-    val color: Color = Color.Unspecified,
-    val iconColor: Color = Color.Unspecified,
-    val iconBackgroundColor: Color = Color.Unspecified,
+    val iconContentDescription: Int = R.string.icon_content_desc,
+    val color: Color,
+    val iconColor: Color? = null,
     val isIconTrailing: Boolean = true,
-    val nightMode: Boolean = false,
+    val iconBackgroundColor: Color,
 )
--- componentsv2/src/main/java/uk/gov/android/ui/componentsv2/text/previewparameterprovider/GdsAnnotatedStringPreviewDataProvider.kt
+++ componentsv2/src/main/java/uk/gov/android/ui/componentsv2/text/previewparameterprovider/GdsAnnotatedStringPreviewDataProvider.kt
@@ -1,48 +1,72 @@
 package uk.gov.android.ui.componentsv2.text.previewparameterprovider
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import uk.gov.android.ui.componentsv2.R
 import uk.gov.android.ui.componentsv2.text.annotatedstringparameters.GdsAnnotatedStringPreviewData
 import uk.gov.android.ui.theme.m3.ExtraTypography
-import uk.gov.android.ui.theme.m3.Typography
 
-class GdsAnnotatedStringPreviewDataProvider :
-    PreviewParameterProvider<GdsAnnotatedStringPreviewData> {
-    override val values: Sequence<GdsAnnotatedStringPreviewData> = sequenceOf(
-        GdsAnnotatedStringPreviewData(
-            text = R.string.annotated_string,
-            fontWeight = FontWeight.Bold,
-            icon = R.drawable.ic_external_site,
-            iconContentDescription = R.string.icon_content_desc,
-            isIconTrailing = true,
-            iconColor = Color.Green,
-        ),
-        GdsAnnotatedStringPreviewData(
-            text = R.string.annotated_string,
-            fontWeight = FontWeight.Bold,
-            icon = R.drawable.ic_error_filled,
-            iconContentDescription = R.string.icon_content_desc,
-            isIconTrailing = false,
-            iconColor = Color.Black,
-            nightMode = true,
-        ),
-        GdsAnnotatedStringPreviewData(
-            text = R.string.annotated_string,
-            fontWeight = FontWeight.Normal,
-            icon = R.drawable.ic_external_site,
-            iconContentDescription = R.string.icon_content_desc,
-            textStyle = Typography.bodyLarge,
-        ),
-        GdsAnnotatedStringPreviewData(
-            text = R.string.annotated_string,
-            fontWeight = FontWeight.Bold,
-            icon = R.drawable.ic_external_site,
-            iconContentDescription = R.string.icon_content_desc,
-            textStyle = ExtraTypography.bodyLargeBold,
-            color = Color.Green,
-            iconColor = Color.Green,
-        ),
+internal enum class GdsAnnotatedStringPreview {
+    TrailingIcon,
+    LeadingIcon,
+    CustomTextStyle,
+    BoldFontWeight,
+    CustomColors,
+}
+
+internal class GdsAnnotatedStringPreviewDataProvider :
+    PreviewParameterProvider<GdsAnnotatedStringPreview> {
+    override val values: Sequence<GdsAnnotatedStringPreview> =
+        GdsAnnotatedStringPreview.entries.asSequence()
+}
+
+@Composable
+internal fun GdsAnnotatedStringPreview.toData(): GdsAnnotatedStringPreviewData {
+    val defaultParams = GdsAnnotatedStringPreviewData(
+        text = "Default",
+        color = MaterialTheme.colorScheme.onBackground,
+        iconBackgroundColor = MaterialTheme.colorScheme.background,
     )
+
+    return when (this) {
+        GdsAnnotatedStringPreview.TrailingIcon ->
+            defaultParams.copy(
+                text = "Trailing icon",
+                icon = R.drawable.ic_external_site,
+                iconContentDescription = R.string.icon_content_desc,
+            )
+
+        GdsAnnotatedStringPreview.LeadingIcon ->
+            defaultParams.copy(
+                text = "Leading icon",
+                icon = R.drawable.ic_error_filled,
+                iconContentDescription = R.string.icon_content_desc,
+                isIconTrailing = false,
+            )
+
+        GdsAnnotatedStringPreview.CustomTextStyle ->
+            defaultParams.copy(
+                text = "Custom text style",
+                fontWeight = FontWeight.Normal,
+                textStyle = ExtraTypography.bodyLargeBold,
+            )
+
+        GdsAnnotatedStringPreview.BoldFontWeight ->
+            defaultParams.copy(
+                text = "Bold font weight",
+                fontWeight = FontWeight.Bold,
+            )
+
+        GdsAnnotatedStringPreview.CustomColors ->
+            defaultParams.copy(
+                text = "Custom colors",
+                color = Color.Red,
+                iconColor = Color.Green,
+                iconBackgroundColor = Color.Magenta,
+                textStyle = ExtraTypography.bodyLargeBold,
+            )
+    }
 }
