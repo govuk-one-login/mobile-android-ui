36eea6e8fe7dcfac24fe8f4da64a15ade6d3b265
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-09-18 12:11:41 +0100

    fix: Gds Annotated String Previews
    - fix unused imports

--- componentsv2/src/test/java/uk/gov/android/ui/componentsv2/text/GdsAnnotatedStringScreenshotTest.kt
+++ componentsv2/src/test/java/uk/gov/android/ui/componentsv2/text/GdsAnnotatedStringScreenshotTest.kt
@@ -5,13 +5,12 @@ import com.android.resources.NightMode
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import uk.gov.android.ui.componentsv2.BaseScreenshotTest
-import uk.gov.android.ui.componentsv2.supportingtext.GdsSupportingText
-import uk.gov.android.ui.componentsv2.supportingtext.SupportingTextPreviewProvider
 import uk.gov.android.ui.theme.m3.GdsTheme
 
 @RunWith(Parameterized::class)
-internal class GdsAnnotatedStringScreenshotTest(private val parameters: Pair<GdsAnnotatedStringPreview, NightMode>) :
-    BaseScreenshotTest(parameters.second) {
+internal class GdsAnnotatedStringScreenshotTest(
+    private val parameters: Pair<GdsAnnotatedStringPreview, NightMode>,
+) : BaseScreenshotTest(parameters.second) {
 
     override val generateComposeLayout: @Composable () -> Unit = {
         val parameters = parameters.first
